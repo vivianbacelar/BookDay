45c63a5d9be4688551f46cf27148478207ba8a12
vivianbacelar <vivianbacoli@gmail.com>
2023-01-24 18:26:00 -0300

    Controller

--- BookDay/BookSearchManager.swift
+++ BookDay/BookSearchManager.swift
@@ -9,9 +9,16 @@ import Foundation
 
 class MyRequestController{
     func sendRequest(){
-        
-        
+        /* Configure session, choose between:
+         * defaultSessionConfiguration
+         * ephemeralSessionConfiguration
+         * backgroundSessionConfigurationWithIdentifier:
+         And set session-wide properties, such as: HTTPAdditionalHeaders,
+         HTTPCookieAcceptPolicy, requestCachePolicy or timeoutIntervalForRequest.
+         */
         let sessionConfig = URLSessionConfiguration.default
+        
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
         
         
         
