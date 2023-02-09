54759e325d4a6072d4e88e4c32db986b5f7987f9
vivianbacelar <vivianbacoli@gmail.com>
2023-02-09 16:54:37 -0300

    Commit terceira vez

--- BookDay/View/Abandon.swift
+++ BookDay/View/Abandon.swift
@@ -5,20 +5,20 @@
 //  Created by Vivian Bacelar on 09/02/23.
 //
 
-import SwiftUI
-
-struct Abandon: View {
-    var body: some View {
-       
-        
-        
-        
-        
-    }
-}
-
-struct Abandon_Previews: PreviewProvider {
-    static var previews: some View {
-        Abandon()
-    }
-}
+//import SwiftUI
+//
+//struct Abandon: View {
+//    var body: some View {
+//       
+//        
+//        
+//        
+//        
+//    }
+//}
+//
+//struct Abandon_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Abandon()
+//    }
+//}
