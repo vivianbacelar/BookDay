c98ecca420ea20bd19124ff4af0337483baa9569
Isabella <Bellagomesp2003@gmail.com>
2023-02-14 14:42:29 -0300

    Paddings todos certos

--- BookDay/View/Components/ContentCell.swift
+++ BookDay/View/Components/ContentCell.swift
@@ -21,7 +21,7 @@ struct ContentCell: View {
                     Text ("Description")
                         .font(.custom("Raleway", size: 18).weight(.bold))
                         .foregroundColor(Color.corPreta)
-                        .padding (.leading, 16)
+                        .padding (.leading, UIScreen.main.bounds.height/30)
                         .padding(.top)
 
                     Spacer()
@@ -29,13 +29,13 @@ struct ContentCell: View {
                     Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
                         .foregroundColor(Color.corRosa)
                         .font(.system(size: 22, weight: .regular))
-                        .padding(.trailing, 16)
+                        .padding(.trailing, UIScreen.main.bounds.width/10)
                 }
                 if isExpanded {
                     VStack{
                         LabelAlignment(text: item.volumeInfo.description ?? "", textAlignmentStyle: .justified, width: UIScreen.main.bounds.width - 40, color: UIColor(Color.corCinzaEscuro), font: UIFont(name: "Raleway", size: 15)!)
-                            .padding(. top, 16)
-                            .padding (.horizontal, 16)
+                            .padding(. top, UIScreen.main.bounds.height/35)
+                            .padding (.horizontal,  UIScreen.main.bounds.width/15)
                     }
                 }
                 Spacer()
